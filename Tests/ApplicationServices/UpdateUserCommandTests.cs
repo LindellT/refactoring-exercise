80df8b21ab1e60772e6fa24b63bfce4bfb4e0187
Thomas Lindell <thomas.lindell@goto.com>
2023-03-20 10:53:55 +0200

    add tests for updateusercommand

---
+++ Tests/ApplicationServices/UpdateUserCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using ApplicationServices;
+using Domain;
+
+namespace Tests.ApplicationServices;
+
+internal class UpdateUserCommandTests
+{
+    [Test]
+    public void GivenSmartConstructerIsCalled_WhenParametersAreNotValid_ThenReturnsNull()
+    {
+        // Arrange
+        var userId = 1;
+        var email = ValidEmailAddress.CreateFrom(null);
+        var password = ValidPassword.CreateFrom(null);
+
+        // Act
+        var result = UpdateUserCommand.CreateFrom(userId, email, password);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Test]
+    [TestCase("bill@microsoft.com", "password123")]
+    [TestCase("bill@microsoft.com", null)]
+    [TestCase(null, "password123")]
+    public void GivenSmartConstructerIsCalled_WhenParametersAreValid_ThenReturnsCorrectly(string? email, string? password)
+    {
+        // Arrange
+        var userId = 1;
+        var validEmail = ValidEmailAddress.CreateFrom(email);
+        var validPassword = ValidPassword.CreateFrom(password);
+
+        // Act
+        var result = UpdateUserCommand.CreateFrom(userId, validEmail, validPassword);
+
+        // Assert
+        result.Should().BeOfType<UpdateUserCommand>().Which.Should().BeEquivalentTo(
+            new 
+            {
+                Id = userId,
+                EmailAddress = validEmail,
+                Password = validPassword,
+            });
+    }
+}
