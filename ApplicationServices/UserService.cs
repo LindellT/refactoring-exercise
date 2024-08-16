315fe18602c4ec3f9158891b5de6ff367ea2ad94
Thomas Lindell <thomas.lindell@goto.com>
2024-08-16 14:54:44 +0300

    small refactor to update user + formatting fixes

--- ApplicationServices/UserService.cs
+++ ApplicationServices/UserService.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-
-namespace ApplicationServices;
+﻿namespace ApplicationServices;
 
 internal sealed class UserService(IUserRepository userRepository) : IUserService
 {
@@ -11,16 +9,14 @@ internal sealed class UserService(IUserRepository userRepository) : IUserService
             passwordSaltValidationError => throw passwordSaltValidationError);
 
     public async Task<OneOf<Success<int>, EmailReservedError, UserCreationFailedError>> CreateUserAsync(CreateUserCommand command, CancellationToken cancellationToken)
-    {
-        return await (await _userRepository.FindUserByEmailAsync(command.EmailAddress, cancellationToken))
+        => await (await _userRepository.FindUserByEmailAsync(command.EmailAddress, cancellationToken))
             .Match<Task<OneOf<Success<int>, EmailReservedError, UserCreationFailedError>>>(
-                async res => await Task.FromResult(new EmailReservedError()),
+                async found => await Task.FromResult(new EmailReservedError()),
                 async _ => (await SaveUserAsync(command, cancellationToken))
                     .Match<OneOf<Success<int>, EmailReservedError, UserCreationFailedError>>(
                         success => success,
                         error => error)
                 );
-    }
 
     private async Task<OneOf<Success<int>, UserCreationFailedError>> SaveUserAsync(CreateUserCommand command, CancellationToken cancellationToken)
         => await _userRepository.CreateUserAsync(
@@ -41,37 +37,33 @@ internal sealed class UserService(IUserRepository userRepository) : IUserService
         => (await _userRepository.ListUsersAsync(cancellationToken)).Select(u => UserDTO.FromUser(u)!).ToList();
 
     public async Task<OneOf<Success, NotFound, EmailReservedError, UserUpdateFailedError>> UpdateUserAsync(UpdateUserCommand command, CancellationToken cancellationToken)
-    {
-        User? user = null;
-        (await _userRepository.FindUserAsync(command.Id, cancellationToken)).Switch(res => user = res, _ => user = null);
+        => await (await _userRepository.FindUserAsync(command.Id, cancellationToken))
+            .Match<Task<OneOf<Success, NotFound, EmailReservedError, UserUpdateFailedError>>>(
+                async user =>
+                {
+                    if (command.EmailAddress is not null)
+                    {
+                        User? userByEmail = null;
+                        (await _userRepository.FindUserByEmailAsync(command.EmailAddress, cancellationToken)).Switch(res => userByEmail = res, _ => userByEmail = null);
 
-        if (user is null)
-        {
-            return new NotFound();
-        }
+                        if (userByEmail is not null && userByEmail.Id != command.Id)
+                        {
+                            return new EmailReservedError();
+                        }
 
-        if (command.EmailAddress is not null)
-        {
-            User? userByEmail = null;
-            (await _userRepository.FindUserByEmailAsync(command.EmailAddress, cancellationToken)).Switch(res => userByEmail = res, _ => userByEmail = null);
+                        user = user with { Email = command.EmailAddress, };
+                    }
 
-            if (userByEmail is not null && userByEmail.Id != command.Id)
-            {
-                return new EmailReservedError();
-            }
+                    if (command.Password is not null)
+                    {
+                        user = user with { HashedPassword = HashedPassword.CreateFrom(command.Password, _validPasswordSalt), };
+                    }
 
-            user = user with { Email = command.EmailAddress, };
-        }
-
-        if (command.Password is not null)
-        {
-            user = user with { HashedPassword = HashedPassword.CreateFrom(command.Password, _validPasswordSalt), };
-        }
-
-        return (await _userRepository.UpdateUserAsync(user, cancellationToken))
-            .Match<OneOf<Success, NotFound, EmailReservedError, UserUpdateFailedError>>(
-                success => success,
-                notFound => notFound,
-                userUpdateFailedError => userUpdateFailedError);
-    }
+                    return (await _userRepository.UpdateUserAsync(user, cancellationToken))
+                        .Match<OneOf<Success, NotFound, EmailReservedError, UserUpdateFailedError>>(
+                            success => success,
+                            notFound => notFound,
+                            userUpdateFailedError => userUpdateFailedError);
+                },
+                async _ => await Task.FromResult(new NotFound()));
 }
