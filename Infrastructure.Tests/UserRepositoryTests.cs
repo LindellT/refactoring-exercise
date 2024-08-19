3791ac981a18786771c154353aaa333a7d6bd1a7
Thomas Lindell <thomas.lindell@goto.com>
2024-08-19 15:20:19 +0300

    add GivenDeleteUserAsyncIsCalled_WhenDeleted_ThenReturnsCorrectly

--- Infrastructure.Tests/UserRepositoryTests.cs
+++ Infrastructure.Tests/UserRepositoryTests.cs
@@ -32,35 +32,49 @@ public class UserRepositoryTests
     [Fact]
     public async Task GivenCreateUserAsyncIsCalled_WhenCreationFails_ThenReturnsCorrectly()
     {
-		// Arrange
-		var email = ValidEmailAddress.CreateFrom("bill@microsoft.com").Match<ValidEmailAddress?>(validEmailAddress => validEmailAddress, emailValidationError => null)!;
-		var password = ValidPassword.CreateFrom("password123").Match<ValidPassword?>(validPassword => validPassword, passwordValidationError => null)!;
-		var passwordSalt = ValidPasswordSalt.CreateFrom("12345678901235467890123456789012").Match<ValidPasswordSalt?>(validPasswordSalt => validPasswordSalt, passwordSaltValidationError => null)!;
-		var passwordHash = HashedPassword.CreateFrom(password, passwordSalt)!;
-		using SqliteConnection connection = new("Filename=:memory:");
-		var contextOptions = new DbContextOptionsBuilder<UserContext>()
-			.UseSqlite(connection)
-			.Options;
-		using var context = Substitute.ForPartsOf<UserContext>(contextOptions);
-        context.SaveChangesAsync(Arg.Any<CancellationToken>()).Returns(0);
-		var sut = new UserRepository(context);
-
-		// Act
-		var result = (await sut.CreateUserAsync(email, passwordHash, CancellationToken.None)).Match<UserCreationFailedError?>(success => null, userCreationFailedError => userCreationFailedError);
-
-		// Assert
-		result.Should().NotBeNull().And.BeOfType<UserCreationFailedError>();
-	}
-
-    [Fact]
-    public void GivenDeleteUserAsyncIsCalled_WhenDeleted_ThenReturnsCorrectly()
-    {
         // Arrange
+        var email = ValidEmailAddress.CreateFrom("bill@microsoft.com").Match<ValidEmailAddress?>(validEmailAddress => validEmailAddress, emailValidationError => null)!;
+        var password = ValidPassword.CreateFrom("password123").Match<ValidPassword?>(validPassword => validPassword, passwordValidationError => null)!;
+        var passwordSalt = ValidPasswordSalt.CreateFrom("12345678901235467890123456789012").Match<ValidPasswordSalt?>(validPasswordSalt => validPasswordSalt, passwordSaltValidationError => null)!;
+        var passwordHash = HashedPassword.CreateFrom(password, passwordSalt)!;
+        using SqliteConnection connection = new("Filename=:memory:");
+        var contextOptions = new DbContextOptionsBuilder<UserContext>()
+            .UseSqlite(connection)
+            .Options;
+        using var context = Substitute.ForPartsOf<UserContext>(contextOptions);
+        context.SaveChangesAsync(Arg.Any<CancellationToken>()).Returns(0);
+        var sut = new UserRepository(context);
 
         // Act
+        var result = (await sut.CreateUserAsync(email, passwordHash, CancellationToken.None)).Match<UserCreationFailedError?>(success => null, userCreationFailedError => userCreationFailedError);
 
         // Assert
-        throw new NotImplementedException();
+        result.Should().NotBeNull().And.BeOfType<UserCreationFailedError>();
+    }
+
+    [Fact]
+    public async Task GivenDeleteUserAsyncIsCalled_WhenDeleted_ThenReturnsCorrectly()
+    {
+        // Arrange
+        var email = ValidEmailAddress.CreateFrom("bill@microsoft.com").Match<ValidEmailAddress?>(validEmailAddress => validEmailAddress, emailValidationError => null)!;
+        var password = ValidPassword.CreateFrom("password123").Match<ValidPassword?>(validPassword => validPassword, passwordValidationError => null)!;
+        var passwordSalt = ValidPasswordSalt.CreateFrom("12345678901235467890123456789012").Match<ValidPasswordSalt?>(validPasswordSalt => validPasswordSalt, passwordSaltValidationError => null)!;
+        var passwordHash = HashedPassword.CreateFrom(password, passwordSalt)!;
+        using SqliteConnection connection = new("Filename=:memory:");
+        await connection.OpenAsync();
+        var contextOptions = new DbContextOptionsBuilder<UserContext>()
+            .UseSqlite(connection)
+            .Options;
+        using UserContext context = new(contextOptions);
+        await context.Database.MigrateAsync();
+        var sut = new UserRepository(context);
+        var userId = (await sut.CreateUserAsync(email, passwordHash, CancellationToken.None)).Match<Success<int>?>(success => success, userCreationFailedError => null)!.Value.Value;
+
+        // Act
+        var result = (await sut.DeleteUserAsync(userId, CancellationToken.None)).Match<Success?>(success => success, notFound => null, userCreationFailedError => null);
+
+        // Assert
+        result.Should().NotBeNull().And.BeOfType<Success>();
     }
 
     [Fact]
