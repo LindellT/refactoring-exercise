6b2b4efe5d92645f1a90b6dadb54e0e06e9557a1
Thomas Lindell <thomas.lindell@goto.com>
2024-08-19 10:44:05 +0300

    add test project for infrastructure

---
+++ Infrastructure.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using Domain;
+global using Infrastructure;
+global using ApplicationServices;
+global using FluentAssertions;
+global using NSubstitute;
+global using OneOf;
+global using OneOf.Types;
